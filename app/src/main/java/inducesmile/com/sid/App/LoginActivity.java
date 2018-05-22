2ca41a8b39aa3175829bb6b7c89d5e993e8353fb
mmmoo1-iul <mmmoo1@iscte-iul.pt>
2018-05-22 22:04:13 +0100

    Icone excessivo removido

--- app/src/main/java/inducesmile/com/sid/App/LoginActivity.java
+++ app/src/main/java/inducesmile/com/sid/App/LoginActivity.java
@@ -24,19 +24,19 @@ public class LoginActivity extends AppCompatActivity {
         port = sp1.getString("port", null);
         username = sp1.getString("Unm", null);
         password = sp1.getString("Psw", null);
-        ((EditText)(findViewById(R.id.username))).setText(username);
-        ((EditText)(findViewById(R.id.password))).setText(password);
-        ((EditText)(findViewById(R.id.ip))).setText(ip);
-        ((EditText)(findViewById(R.id.port))).setText(port);
+        ((EditText) (findViewById(R.id.username))).setText(username);
+        ((EditText) (findViewById(R.id.password))).setText(password);
+        ((EditText) (findViewById(R.id.ip))).setText(ip);
+        ((EditText) (findViewById(R.id.port))).setText(port);
     }
 
     public void loginClick(View v) {
         SharedPreferences sp = getSharedPreferences("Login", MODE_PRIVATE);
         SharedPreferences.Editor Ed = sp.edit();
-        Ed.putString("Unm", "username");
-        Ed.putString("Psw", ((EditText)(findViewById(R.id.password))).getText().toString());
-        Ed.putString("ip", ((EditText)(findViewById(R.id.ip))).getText().toString());
-        Ed.putString("port", ((EditText)(findViewById(R.id.port))).getText().toString());
+        Ed.putString("Unm", ((EditText) (findViewById(R.id.username))).getText().toString());
+        Ed.putString("Psw", ((EditText) (findViewById(R.id.password))).getText().toString());
+        Ed.putString("ip", ((EditText) (findViewById(R.id.ip))).getText().toString());
+        Ed.putString("port", ((EditText) (findViewById(R.id.port))).getText().toString());
         Ed.apply();
         new UserLogin(ip, port, username, password);
         Intent i = new Intent(this, MainActivity.class);
